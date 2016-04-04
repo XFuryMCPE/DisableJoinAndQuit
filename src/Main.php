075fbbeb952236169a7120c3d954daa89f09fd92
Syntaxious <SynPlaysMC@gmail.com>
2016-04-04 15:32:06 +0100

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DisableJoinAndQuit;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+	
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+   
+    public function onJoin(PlayerJoinEvent $event){
+        $event->setJoinMessage("");
+    }
+   
+    public function onQuit(PlayerQuitEvent $event){
+        $event->setQuitMessage("");
+    }
+}
+?>
